c06d3181cd53bd9336f74416f5008e3cd7fc2012
Pascal Jobin <32720778+jobinpa@users.noreply.github.com>
2021-12-08 21:13:44 -0600

    Updated lesson 23 homework

--- openCV-L23H-Pose face and landmarks.py
+++ openCV-L23H-Pose face and landmarks.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipehelper as mph
+import numpy as np
 print(f'OpenCV version is {cv2.__version__}')
 
 # Parameters
@@ -11,6 +12,7 @@ FLIP_CAMERA_FRAME_HORIZONTALY = True
 
 WINDOW_CAMERA_POS = (0, 0)
 WINDOW_CAMERA_NAME = 'Camera'
+WINDOW_BLACK_NAME = 'Black'
 
 # Setup camera
 cam = cv2.VideoCapture(CAM_ID, cv2.CAP_DSHOW)
@@ -33,9 +35,13 @@ cv2.namedWindow(WINDOW_CAMERA_NAME)
 cv2.moveWindow(WINDOW_CAMERA_NAME, WINDOW_CAMERA_POS[0], WINDOW_CAMERA_POS[1])
 cv2.resizeWindow(WINDOW_CAMERA_NAME, frameDim[0], frameDim[1])
 
+cv2.namedWindow(WINDOW_BLACK_NAME)
+cv2.moveWindow(WINDOW_BLACK_NAME, WINDOW_CAMERA_POS[0] + frameDim[0], WINDOW_CAMERA_POS[1])
+cv2.resizeWindow(WINDOW_BLACK_NAME, frameDim[0], frameDim[1])
+
 print('Press "q" to quit...')
 
-faceDetection = mph.FaceMeshDetection(min_detection_confidence=0.5)
+faceDetection = mph.FaceMeshDetection(min_detection_confidence=0.5, max_num_faces=5)
 faceRegions = [
     mph.FACEMESH_REGION_SILHOUETTE,
     mph.FACEMESH_REGION_LEFT_EYE_UPPER_0,
@@ -59,12 +65,15 @@ while True:
 
     if FLIP_CAMERA_FRAME_HORIZONTALY:
         frame = cv2.flip(frame, 1)
+    frameBlack = np.zeros([frameDim[1], frameDim[0], 3], dtype=np.uint8)
 
     for faceMesh in faceDetection.detectFaces(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)):
         for lm in faceMesh.getLandmarks(*faceRegions):
             cv2.circle(frame, lm, 1, (0, 0, 255), -1)
+            cv2.circle(frameBlack, lm, 1, (0, 0, 255), -1)
 
     cv2.imshow(WINDOW_CAMERA_NAME, frame)
+    cv2.imshow(WINDOW_BLACK_NAME, frameBlack)
 
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
