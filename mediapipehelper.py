907f2226b43a71f065394df103b94ef92119580f
Pascal Jobin <32720778+jobinpa@users.noreply.github.com>
2021-12-08 19:15:12 -0600

    Added lesson 23 homework

--- mediapipehelper.py
+++ mediapipehelper.py
@@ -3,6 +3,38 @@ import mediapipe as mp
 LEFT_HAND = 0
 RIGHT_HAND = 1
 
+# https://github.com/tensorflow/tfjs-models/commit/838611c02f51159afdd77469ce67f0e26b7bbb23
+FACEMESH_REGION_ALL                 = 0
+FACEMESH_REGION_SILHOUETTE          = 1
+FACEMESH_REGION_LIPS_UPPER_OUTER    = 2
+FACEMESH_REGION_LIPS_LOWER_OUTER    = 3
+FACEMESH_REGION_LIPS_UPPER_INNER    = 4
+FACEMESH_REGION_LIPS_LOWER_INNER    = 5
+FACEMESH_REGION_RIGHT_EYE_UPPER_0   = 6
+FACEMESH_REGION_RIGHT_EYE_LOWER_0   = 7
+FACEMESH_REGION_RIGHT_EYE_UPPER_1   = 8
+FACEMESH_REGION_RIGHT_EYE_LOWER_1   = 9
+FACEMESH_REGION_RIGHT_EYE_UPPER_2   = 10
+FACEMESH_REGION_RIGHT_EYE_LOWER_2   = 11
+FACEMESH_REGION_RIGHT_EYE_LOWER_3   = 12
+FACEMESH_REGION_RIGHT_EYEBROW_UPPER = 13
+FACEMESH_REGION_RIGHT_EYEBROW_LOWER = 14
+FACEMESH_REGION_LEFT_EYE_UPPER_0    = 15
+FACEMESH_REGION_LEFT_EYE_LOWER_0    = 16
+FACEMESH_REGION_LEFT_EYE_UPPER_1    = 17
+FACEMESH_REGION_LEFT_EYE_LOWER_1    = 18
+FACEMESH_REGION_LEFT_EYE_UPPER_2    = 19
+FACEMESH_REGION_LEFT_EYE_LOWER_2    = 20
+FACEMESH_REGION_LEFT_EYE_LOWER_3    = 21
+FACEMESH_REGION_LEFT_EYEBROW_UPPER  = 22
+FACEMESH_REGION_LEFT_EYEBROW_LOWER  = 23
+FACEMESH_REGION_MIDWAY_BETWEEN_EYES = 24
+FACEMESH_REGION_NOSE_TIP            = 25
+FACEMESH_REGION_NOSE_BOTTOM         = 26
+FACEMESH_REGION_NOSE_RIGHT_CORNER   = 27
+FACEMESH_REGION_NOSE_LEFT_CORNER    = 28
+FACEMESH_REGION_RIGHT_CHEEK         = 29
+FACEMESH_REGION_LEFT_CHEEK          = 30
 
 class Face:
     def __init__(self, boxUpperLeft, boxLowerRight):
@@ -15,6 +47,64 @@ class Face:
     def getBoxLowerRight(self):
         return self.__boxLowerRight__
 
+class FaceMesh:
+    # https://github.com/tensorflow/tfjs-models/commit/838611c02f51159afdd77469ce67f0e26b7bbb23
+    __regionIndexes__ = {
+        FACEMESH_REGION_ALL                 : range(0, 468),
+        FACEMESH_REGION_SILHOUETTE          : [
+                                                10,  338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288,
+                                                397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136,
+                                                172, 58,  132, 93,  234, 127, 162, 21,  54,  103, 67,  109
+                                              ],
+        FACEMESH_REGION_LIPS_UPPER_OUTER    : [61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291],
+        FACEMESH_REGION_LIPS_LOWER_OUTER    : [146, 91, 181, 84, 17, 314, 405, 321, 375, 291],
+        FACEMESH_REGION_LIPS_UPPER_INNER    : [78, 191, 80, 81, 82, 13, 312, 311, 310, 415, 308],
+        FACEMESH_REGION_LIPS_LOWER_INNER    : [78, 95, 88, 178, 87, 14, 317, 402, 318, 324, 308],
+        FACEMESH_REGION_RIGHT_EYE_UPPER_0   : [246, 161, 160, 159, 158, 157, 173],
+        FACEMESH_REGION_RIGHT_EYE_LOWER_0   : [33, 7, 163, 144, 145, 153, 154, 155, 133],
+        FACEMESH_REGION_RIGHT_EYE_UPPER_1   : [247, 30, 29, 27, 28, 56, 190],
+        FACEMESH_REGION_RIGHT_EYE_LOWER_1   : [130, 25, 110, 24, 23, 22, 26, 112, 243],
+        FACEMESH_REGION_RIGHT_EYE_UPPER_2   : [113, 225, 224, 223, 222, 221, 189],
+        FACEMESH_REGION_RIGHT_EYE_LOWER_2   : [226, 31, 228, 229, 230, 231, 232, 233, 244],
+        FACEMESH_REGION_RIGHT_EYE_LOWER_3   : [143, 111, 117, 118, 119, 120, 121, 128, 245],
+        FACEMESH_REGION_RIGHT_EYEBROW_UPPER : [156, 70, 63, 105, 66, 107, 55, 193],
+        FACEMESH_REGION_RIGHT_EYEBROW_LOWER : [35, 124, 46, 53, 52, 65],
+        FACEMESH_REGION_LEFT_EYE_UPPER_0    : [466, 388, 387, 386, 385, 384, 398],
+        FACEMESH_REGION_LEFT_EYE_LOWER_0    : [263, 249, 390, 373, 374, 380, 381, 382, 362],
+        FACEMESH_REGION_LEFT_EYE_UPPER_1    : [467, 260, 259, 257, 258, 286, 414],
+        FACEMESH_REGION_LEFT_EYE_LOWER_1    : [359, 255, 339, 254, 253, 252, 256, 341, 463],
+        FACEMESH_REGION_LEFT_EYE_UPPER_2    : [342, 445, 444, 443, 442, 441, 413],
+        FACEMESH_REGION_LEFT_EYE_LOWER_2    : [446, 261, 448, 449, 450, 451, 452, 453, 464],
+        FACEMESH_REGION_LEFT_EYE_LOWER_3    : [372, 340, 346, 347, 348, 349, 350, 357, 465],
+        FACEMESH_REGION_LEFT_EYEBROW_UPPER  : [383, 300, 293, 334, 296, 336, 285, 417],
+        FACEMESH_REGION_LEFT_EYEBROW_LOWER  : [265, 353, 276, 283, 282, 295],
+        FACEMESH_REGION_MIDWAY_BETWEEN_EYES : [168],
+        FACEMESH_REGION_NOSE_TIP            : [1],
+        FACEMESH_REGION_NOSE_BOTTOM         : [2],
+        FACEMESH_REGION_NOSE_RIGHT_CORNER   : [98],
+        FACEMESH_REGION_NOSE_LEFT_CORNER    : [327],
+        FACEMESH_REGION_RIGHT_CHEEK         : [205],
+        FACEMESH_REGION_LEFT_CHEEK          : [425]
+    }
+
+    def __init__(self, landmarks):
+        self.__landmarks__ = landmarks
+
+    def getLandmarks(self, *regions):
+        landmarkIndices = set()
+
+        if (len(regions) == 0):
+            for i in FaceMesh.__regionIndexes__[FACEMESH_REGION_ALL]:
+                landmarkIndices.add(i)
+        else:
+            for r in regions:
+                for i in FaceMesh.__regionIndexes__[r]:
+                    landmarkIndices.add(i)
+
+        arr = []
+        for i in set(landmarkIndices):
+            arr.append(self.__landmarks__[i])
+        return arr
 
 class Hand:
     def __init__(self, markers, handedness):
@@ -179,6 +269,31 @@ class FaceDetection:
                 faces.append(Face(topLeft, bottomRight))
         return faces
 
+class FaceMeshDetection:
+    def __init__(self, static_image_mode=False, max_num_faces=1,
+                 min_detection_confidence=0.5, min_tracking_confidence=0.5):
+        # https://google.github.io/mediapipe/solutions/face_mesh.html
+        self.__mpFaceMesh__ = mp.solutions.face_mesh.FaceMesh(
+            static_image_mode=static_image_mode,
+            max_num_faces=max_num_faces,
+            min_detection_confidence=min_detection_confidence,
+            min_tracking_confidence=min_tracking_confidence
+        )
+
+    def detectFaces(self, frameRGB):
+        dimY = len(frameRGB)
+        dimX = 0 if dimY == 0 else len(frameRGB[0])
+        mpFaceMeshOuput = self.__mpFaceMesh__.process(frameRGB)
+        faces = []
+        if mpFaceMeshOuput.multi_face_landmarks != None:
+            for f in mpFaceMeshOuput.multi_face_landmarks:
+                markers = []
+                for landmark in f.landmark:
+                    lx = int(landmark.x * dimX)
+                    ly = int(landmark.y * dimY)
+                    markers.append((lx, ly))
+                faces.append(FaceMesh(markers))
+        return faces
 
 class HandDetection:
     def __init__(self, static_image_mode=False, max_num_hands=-1,
---
+++ openCV-L23H-Pose face and landmarks.py
@@ -0,0 +1,76 @@
+import cv2
+import mediapipehelper as mph
+print(f'OpenCV version is {cv2.__version__}')
+
+# Parameters
+CAM_ID = 0
+CAM_FPS = 30
+CAM_RES = (640, 480)
+
+FLIP_CAMERA_FRAME_HORIZONTALY = True
+
+WINDOW_CAMERA_POS = (0, 0)
+WINDOW_CAMERA_NAME = 'Camera'
+
+# Setup camera
+cam = cv2.VideoCapture(CAM_ID, cv2.CAP_DSHOW)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, CAM_RES[0])
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_RES[1])
+cam.set(cv2.CAP_PROP_FPS, CAM_FPS)
+cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+cam.set(cv2.CAP_PROP_ZOOM, 100)
+
+# Display camera settings dialog
+#cam.set(cv2.CAP_PROP_SETTINGS, 1)
+
+# Calculate actual frame dimensions. These dimensions may be different than
+# our settings as the camera supports only a subset of possible resolutions.
+frameDim = (int(cam.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+print(f'Actual camera resolution is "{frameDim[0]}x{frameDim[1]}."')
+
+# Setup the window
+cv2.namedWindow(WINDOW_CAMERA_NAME)
+cv2.moveWindow(WINDOW_CAMERA_NAME, WINDOW_CAMERA_POS[0], WINDOW_CAMERA_POS[1])
+cv2.resizeWindow(WINDOW_CAMERA_NAME, frameDim[0], frameDim[1])
+
+print('Press "q" to quit...')
+
+faceDetection = mph.FaceMeshDetection(min_detection_confidence=0.5)
+faceRegions = [
+    mph.FACEMESH_REGION_SILHOUETTE,
+    mph.FACEMESH_REGION_LEFT_EYE_UPPER_0,
+    mph.FACEMESH_REGION_LEFT_EYE_LOWER_0,
+    mph.FACEMESH_REGION_RIGHT_EYE_UPPER_0,
+    mph.FACEMESH_REGION_RIGHT_EYE_LOWER_0,
+    mph.FACEMESH_REGION_LIPS_LOWER_OUTER,
+    mph.FACEMESH_REGION_LIPS_LOWER_INNER,
+    mph.FACEMESH_REGION_LIPS_UPPER_OUTER,
+    mph.FACEMESH_REGION_LIPS_UPPER_INNER,
+    mph.FACEMESH_REGION_NOSE_BOTTOM,
+    mph.FACEMESH_REGION_NOSE_LEFT_CORNER,
+    mph.FACEMESH_REGION_NOSE_RIGHT_CORNER,
+    mph.FACEMESH_REGION_NOSE_TIP,
+    mph.FACEMESH_REGION_MIDWAY_BETWEEN_EYES
+]
+
+# Read and display camera capture
+while True:
+    _, frame = cam.read()
+
+    if FLIP_CAMERA_FRAME_HORIZONTALY:
+        frame = cv2.flip(frame, 1)
+
+    for faceMesh in faceDetection.detectFaces(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)):
+        for lm in faceMesh.getLandmarks(*faceRegions):
+            cv2.circle(frame, lm, 1, (0, 0, 255), -1)
+
+    cv2.imshow(WINDOW_CAMERA_NAME, frame)
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+# Cleanup
+print('Shutting down...')
+cam.release()
+cv2.destroyAllWindows()
+print('Application ended')
